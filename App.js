ff61515d9ff6dfc15f4b608955c7ff73dfc7097a
Suellen Castro <castrosuellenx@gmail.com>
2021-09-14 20:12:32 -0300

    Field

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,13 @@ export default class App extends Component {
         <Text style={styles.instructions}>
           Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
       </View>
     );
   }
