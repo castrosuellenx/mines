12386054f73515785013a210995e82a61ac614bb
Suellen Castro <castrosuellenx@gmail.com>
2021-09-14 21:09:41 -0300

    Flag

--- src/components/Field.js
+++ src/components/Field.js
@@ -4,24 +4,26 @@ import {View, Text, StyleSheet} from 'react-native';
 
 import params from '../params';
 import Mine from './Mine';
+import Flag from './Flag';
 
 export default props => {
-  const {mined, opened, nearMines, exploded} = props;
+  const {mined, opened, nearMines, exploded, flagged} = props;
 
   const styleField = [styles.field];
-
   if (opened) {
     styleField.push(styles.opened);
   }
   if (exploded) {
     styleField.push(styles.exploded);
   }
-  if (styleField.length === 1) {
+  if (flagged) {
+    styleField.push(styles.flagged);
+  }
+  if (!opened && !exploded) {
     styleField.push(styles.regular);
   }
 
   let color = null;
-
   if (nearMines > 0) {
     if (nearMines === 1) {
       color = '#2A28D7';
@@ -45,6 +47,7 @@ export default props => {
         false
       )}
       {mined && opened ? <Mine /> : false}
+      {flagged && !opened ? <Flag /> : false}
     </View>
   );
 };
